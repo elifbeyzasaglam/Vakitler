ebef59a0a6aa91c7976631aa2c27597fe660ad03
Elif Beyza <saglame99@gmail.com>
2023-04-09 14:32:39 +0300

    Initial Commit

---
+++ Vakitler/VakitlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VakitlerApp.swift
+//  Vakitler
+//
+//  Created by ELİF BEYZA SAĞLAM on 9.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct VakitlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
